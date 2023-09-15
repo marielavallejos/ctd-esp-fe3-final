5e43bd0f726d393150b31f17a86dbf3da9b3c6fd
marienv <mariela.n.vallejos@gmail.com>
2023-09-15 02:15:31 -0300

    .

--- pages/preguntas-frecuentes/index.page.tsx
+++ pages/preguntas-frecuentes/index.page.tsx
@@ -1,5 +1,5 @@
 import BodySingle from 'components/layouts/body/single/body-single'
-import FaqAccordion from 'dh-marvel/components/faqs/FaqAccordion'
+import FaqAccordion from '../../components/faqs/FaqAccordion'
 import { NextPage } from 'next'
 import { FaqsType } from 'types/faqs'
 
